8ecf7254d700769083a083f9a009a2c8529998aa
GabrielLima2803 <gabriellima2803@gmail.com>
2023-09-15 15:19:01 -0300

    Fc - Conexão com as imagem no V-For

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <script setup>
-import axios from 'axios';
-import {ref} from 'vue'
-import HeaderPrincipal from '@/components/header/HeaderPrincipal.vue';
+import axios from 'axios'
+import { ref } from 'vue'
+import HeaderPrincipal from '@/components/header/HeaderPrincipal.vue'
 
-const movies = ref([]); 
-
-const apiKey = '92a1cf3ee1f043920c17b8cff26b95e8';
-const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR`;
+const movies = ref([])
+const apiKey = '92a1cf3ee1f043920c17b8cff26b95e8'
+const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR`
 
 axios.get(apiUrl)
   .then((response) => {
-    movies.value = response.data.results; 
+    movies.value = response.data.results
   })
   .catch((error) => {
-    console.error('Erro ao buscar a lista de filmes:', error);
-  });
+    console.error('Erro ao buscar a lista de filmes:', error)
+  })
 
+const getMoviePosterUrl = (posterPath) => {
+  if (posterPath) {
+    return `https://image.tmdb.org/t/p/w500/${posterPath}`
+  }
+  // Retorne uma imagem de fallback ou uma URL vazia se não houver um caminho de pôster.
+  return 'https://raw.githubusercontent.com/koehlersimon/fallback/master/Resources/Public/Images/placeholder.jpg'
+}
 </script>
-
 <template>
-  <header-principal/>
+  <header-principal />
   <div>
     <h1>Lista de Filmes</h1>
     <ul>
@@ -27,6 +32,7 @@ axios.get(apiUrl)
         <h2>{{ movie.title }}</h2>
         <p>{{ movie.overview }}</p>
         <p>{{ movie.release_date }}</p>
+        <img :src="getMoviePosterUrl(movie.poster_path)" alt="" />
       </li>
     </ul>
   </div>
