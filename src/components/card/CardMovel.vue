8834fb94f1ca25c9f14b8602f2fd2d4d8a1944f3
DuskDarkin <lmiguells095@gmail.com>
2023-10-01 11:29:04 -0300

    Fc - Ajustes em bugs

--- src/components/card/CardMovel.vue
+++ src/components/card/CardMovel.vue
@@ -1,34 +1,39 @@
-<template>
-    <carousel :items-to-show="1.5">
-        <slide v-for="slide in 10" :key="slide">
-            {{ slide }}
-        </slide>
+<script setup>
+import { ref, onMounted } from 'vue'
+import api from '@/plugins/axios.js'
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
 
+const movies = ref([]);
+
+onMounted(async () => {
+  const response = await api.get(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=pt-BR&page=1&sort_by=popularity.desc`)
+  movies.value = response.data.results;
+})
+
+const getMoviePosterUrl = (posterPath) => {
+    if (posterPath) {
+      return `https://image.tmdb.org/t/p/w500/${posterPath}`
+    }
+    // Retorne uma imagem de fallback.
+    return 'https://raw.githubusercontent.com/koehlersimon/fallback/master/Resources/Public/Images/placeholder.jpg'
+  }
+
+  
+</script>
+
+<template>
+    <carousel :items-to-show="5">
+        <slide v-for="movie in movies" :key="movie.id">
+            <img :src="getMoviePosterUrl(movie.poster_path)" alt="" width="150"/>
+        </slide>
         <template #addons>
             <navigation />
-            <pagination />
         </template>
     </carousel>
 </template>
   
-<script>
-// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
-import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
-
-export default {
-    name: 'App',
-    components: {
-        Carousel,
-        Slide,
-        Pagination,
-        Navigation,
-    },
-}
-</script>
 
   
-<style scoped>
-
-</style>
+<style scoped></style>
   
