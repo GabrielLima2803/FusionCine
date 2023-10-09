bc35184ba7e4d3554269c71c168d2b02d79d0279
V1T0Rm3ndes <vitor.msalomao@gmail.com>
2023-10-09 11:02:13 -0300

    banners

--- src/components/CarouselOne.vue
+++ src/components/CarouselOne.vue
@@ -10,19 +10,19 @@ const pictures = ref([
     'alt': 'https://raw.githubusercontent.com/koehlersimon/fallback/master/Resources/Public/Images/placeholder.jpg',
   },
   {
-    'src': 'https://ottvsimg.ottvs.com.br/res/banner/7ca84d92-252f-4cb7-87b2-401a8725dba2.jpg',
+    'src': 'https://ottvsimg.ottvs.com.br/res/banner/72d3acaf-87a4-4d28-9a77-2424dc9da85c.jpg',
     'alt': 'https://raw.githubusercontent.com/koehlersimon/fallback/master/Resources/Public/Images/placeholder.jpg',
   },
   {
-    'src': 'https://ottvsimg.ottvs.com.br/res/banner/7ca84d92-252f-4cb7-87b2-401a8725dba2.jpg',
+    'src': 'https://ottvsimg.ottvs.com.br/res/banner/c721686a-64c8-415d-93af-c50555d10b6a.jpg',
     'alt': 'https://raw.githubusercontent.com/koehlersimon/fallback/master/Resources/Public/Images/placeholder.jpg',
   },
   {
-    'src': 'https://ottvsimg.ottvs.com.br/res/banner/7ca84d92-252f-4cb7-87b2-401a8725dba2.jpg',
+    'src': 'https://ottvsimg.ottvs.com.br/res/banner/4ed2b134-5d16-4be0-bc73-4cea3bfaa15a.jpg',
     'alt': 'https://raw.githubusercontent.com/koehlersimon/fallback/master/Resources/Public/Images/placeholder.jpg',
   },
   {
-    'src': 'https://ottvsimg.ottvs.com.br/res/banner/7ca84d92-252f-4cb7-87b2-401a8725dba2.jpg',
+    'src': 'https://ottvsimg.ottvs.com.br/res/banner/67f6300c-5bae-4c48-ba3f-994f563f3729.jpg',
     'alt': 'https://raw.githubusercontent.com/koehlersimon/fallback/master/Resources/Public/Images/placeholder.jpg',
   },
 ])
