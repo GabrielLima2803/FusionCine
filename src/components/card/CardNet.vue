d35a5e315af144d4ae392a13bd104116860c6414
GabrielLima2803 <gabriellima2803@gmail.com>
2023-10-30 16:55:58 -0300

    Fc - Card Hover Css

--- src/components/card/CardNet.vue
+++ src/components/card/CardNet.vue
@@ -29,16 +29,15 @@ const hideInfo = () => {
       <carousel :items-to-show="2.8" class="Margin">
         <slide v-for="(series, index) in tvStore.tvs" :key="series.id">
           <div class="img-card">
-            <div class="img-container" @mouseover="showInfo(index)" @mouseout="hideInfo(index)">
+            <div class="img-container">
               <div class="counter">
                 <p>{{ `${index + 1}` }}</p>
               </div>
-              <img :src="`https://image.tmdb.org/t/p/w500${series.poster_path}`" :alt="series.title" width="150" class="z-3 img" />
+              <img :src="`https://image.tmdb.org/t/p/w500${series.poster_path}`" :alt="series.title" width="150"
+                class="z-3 img" @mouseover="showInfo(index)" @mouseout="hideInfo(index)" />
               <div v-if="infoVisible === index" class="info">
-                <!-- Conteúdo das informações adicionais -->
                 <p>Título: {{ series.title }}</p>
                 <p>Descrição: {{ series.overview }}</p>
-                <!-- Adicione outras informações que desejar -->
               </div>
             </div>
           </div>
@@ -49,12 +48,74 @@ const hideInfo = () => {
       </carousel>
     </div>
   </div>
+  <div class="info2">
+    <div class="container-trailer">
+      <p>sdas</p>
+    </div>
+    <div class="container-bot">
+      <div class="container-icon">
+        <i class="bi bi-play-fill"></i>
+        <i class="bi bi-plus-circle"></i> 
+        <i class="bi bi-heart-fill"></i>
+      </div>
+      <div class="container-info">
+        <p class="text-green mt-3">96% relevante</p>
+        <div class="display mt-3">
+          <p class="idade">14</p>
+          <p class="ml-2 temp">5 temporadas</p>
+          <i class="bi bi-badge-hd-fill ml-2 icon-hd"></i>
+        </div>
+      </div>
+      <div class="container-tv display mt-4">
+        <p>Título</p>
+        <p class="ml-2">Tipo:</p>
+        <p class="ml-2">Genêro:</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+
+.container-bot{
+  padding: 15px;
+}
 .textPrincipal {
   font-size: 36px;
   font-weight: bold;
+}
+.idade{
+  background-color: orangered;
+  border-radius: 4px;
+  padding: 3px;
+}
+
+.display{
+  display: flex;
+}
+.icon-hd
+{
+  margin-top: 2px;
+}
+.temp{
+  font-size: 16px;
+  margin-top: 2px;
+}
+
+.bi-play-fill {
+  font-size: 32px;
+  color: white;
+}
+.bi-heart-fill{
+color: white;
+margin-left: 5px;
+font-size: 27px;
+
+}
+
+.bi-plus-circle {
+  margin-left: 5px;
+  font-size: 28px;
 }
 
 .container-top {
@@ -78,12 +139,13 @@ const hideInfo = () => {
   right: -55px;
   padding: 5px;
   font-weight: bold;
-  font-size: 323.5px; 
+  font-size: 323.5px;
 }
-.img
-{
-  border: 0.5px solid rgb(169, 169, 169); /* Adicione uma borda cinza em volta da imagem */
+
+.img {
+  border: 0.5px solid rgb(169, 169, 169);
 }
+
 .info {
   background-color: rgba(0, 0, 0, 0.7);
   color: white;
@@ -91,12 +153,24 @@ const hideInfo = () => {
   border-radius: 4px;
   position: absolute;
   top: 0;
-  left: 100%; /* Coloca à direita da imagem */
-  transform: translateX(10px); /* Adiciona um pequeno espaço entre a imagem e as informações */
+  left: 100%;
+  transform: translateX(10px);
   display: none;
 }
 
-.img-container:hover .info {
-  display: block; /* Exibe as informações quando o mouse passa por cima do .img-container */
+.info2 {
+  background-color: black;
+  color: white;
+  width: 350px;
+  height: 330px;
+  border-radius: 4px;
 }
-</style>
+
+.container-trailer {
+  background-color: gray;
+  height: 150px;
+}
+
+.img-container:hover .info {
+  display: block;
+}</style>
--- src/components/footer/SecondaryFooter.vue
+++ src/components/footer/SecondaryFooter.vue
@@ -2,7 +2,7 @@
 
     <div class="margi footer-bot">
     <div class="">
-        <p> © 2023 FusionCIne ©Copyright 2023 FUSION CINE.</p>
+        <p> © 2023 FusionCine ©Copyright 2023 FUSION CINE.</p>
     </div>
     </div>
   </template>
