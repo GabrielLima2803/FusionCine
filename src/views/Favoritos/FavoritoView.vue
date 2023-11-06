023a84bdc4fb178c4151fc05407f022d0613af79
V1T0Rm3ndes <vitor.msalomao@gmail.com>
2023-11-06 11:15:48 -0300

    Fc - Add Css Page Favoritos

--- src/views/Favoritos/FavoritoView.vue
+++ src/views/Favoritos/FavoritoView.vue
@@ -1,13 +1,21 @@
 <script setup>
-
+import HeaderPrincipal from '@/components/header/HeaderPrincipal.vue';
+import FullFooter from '@/components/footer/FullFooter.vue';
 </script>
 
 <template>
-    <div>
-
+    <header-principal/>
+    <div class="favoritos">
+       <h1>Favoritos ()</h1>
     </div>
+    <full-footer/>
 </template>
 
 <style scoped>
-
+.favoritos{
+    font-size: 30px;
+    max-width: 1220px;
+    margin: 0 auto;
+    margin-top: 125px;
+}
 </style>
