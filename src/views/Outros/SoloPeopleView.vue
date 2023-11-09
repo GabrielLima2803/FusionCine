4f225aa0e491dad0e83b1f0f9572e1799b461af2
V1T0Rm3ndes <vitor.msalomao@gmail.com>
2023-11-09 11:21:44 -0300

    Fc - Add css page SoloPeopleView

--- src/views/Outros/SoloPeopleView.vue
+++ src/views/Outros/SoloPeopleView.vue
@@ -34,9 +34,55 @@ onMounted(async () => {
 <template>
     <div>
         <HeaderPrincipal/>
-        <div v-for="person in peoples" :key="person.id">
-            {{ person.name }}
+        <div class="container-max">
+            <div v-for="person in peoples" :key="person.id">
+                <div class="display">
+                    <div class="container-img">
+                        <img :src="`https://image.tmdb.org/t/p/w500${person.profile_path}`" :alt="`Imagem de ${person.name}`"
+                            class="img-profile" width="300" height="450" />
+                    </div>
+                    <div class="info-person">
+                        <h1 class="name">
+                            {{ person.name }}
+                        </h1>
+                        <div class="descrition">
+                            <h2 class="biografia" >Biografia</h2>
+                            <p class="mt-3">Descrição</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
         </div>
         <FullFooter/>
     </div>
 </template> 
+
+<style scoped>
+.container-max{
+    max-width: 1420px;
+    margin: 0 auto;
+    margin-top: 125px; 
+}
+.name{
+    font-size: 30px;
+    font-weight: bold;
+}
+.img-profile{
+    border-radius: 10px;
+}
+.display{
+display: flex;
+
+}
+.info-person{
+    margin-left: 30px;
+}
+.descrition{
+    margin-top: 30px;
+}
+.biografia{
+    font-weight: bold;
+    font-size: 20px;
+}
+</style>
