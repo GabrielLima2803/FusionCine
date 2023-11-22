4f45fbefd09648e57d93f3d5e6e653be92c6a2de
GabrielLima2803 <gabriellima2803@gmail.com>
2023-11-22 16:51:32 -0300

    Fc - Add Fallback page SoloSerie and add css

--- src/views/Filmes&Series/SoloSerieView.vue
+++ src/views/Filmes&Series/SoloSerieView.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { onMounted, ref } from 'vue';
-import { format } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
 import api from '@/plugins/axios';
 import HeaderPrincipal from '@/components/header/HeaderPrincipal.vue';
 import FullFooter from '@/components/footer/FullFooter.vue';
@@ -22,24 +20,13 @@ onMounted(async () => {
 
 const fetchtvDetails = async (id) => {
     try {
-        const response = await api.get(`tv/${id}?language=pt-BR&append_to_response=credits,videos,keywords`, {
+        const response = await api.get(`tv/${id}?language=pt-BR&append_to_response=credits,videos,keywords,imagens`, {
         });
         return response.data;
     } catch (error) {
         console.error('Erro ao buscar os detalhes do filme:', error);
         return {};
     }
-};
-
-const formatDate = (date) => {
-    const formattedDate = format(new Date(date), 'dd MMMM yyyy', { locale: ptBR });
-    return formattedDate.replace(/ /g, ' de ');
-};
-
-const getFormattedRuntime = (runtime) => {
-    const hours = Math.floor(runtime / 60);
-    const minutes = runtime % 60;
-    return `${hours}h ${minutes}min`;
 };
 
 const getGenreNames = (genres) => {
@@ -63,9 +50,14 @@ const toggleShowMore = () => {
 const getKeywordsNames = (keywords) => {
     return keywords.results.map(keyword => keyword.name);
 };
-const getNameOringinal = () => {
-    
-}
+const getDirectorName = (crew) => {
+    const director = crew.find(member => member.job === 'Director');
+    return director ? director.name : 'Não disponível';
+};
+
+const getWriters = (crew) => {
+    return crew.filter(member => member.department === 'Writing');
+};
 </script>
 
 <template>
@@ -81,9 +73,8 @@ const getNameOringinal = () => {
                         <div class="info-tv">
                             <h1 class="title">{{ tv.name }}</h1>
                             <div class="container-so">
-                                <p class="tv-date mt-3 mb-3">{{  }} </p>
+                                <p class="tv-date mt-3 mb-3">{{ }} </p>
                                 <p class="mt-3 ml-3"> • {{ getGenreNames(tv.genres) }} </p>
-                                <!-- <p class="mt-3 ml-3"> • {{ tv.runtime }}</p> -->
                             </div>
                             <div class="trailer mt-5">
                                 <button @click="openTrailer(tv.videos && tv.videos.results)">
@@ -101,13 +92,22 @@ const getNameOringinal = () => {
                                 <i class="bi bi-heart-fill"></i>
                                 <button class="ml-3 btn-1">Alugar</button>
                             </div>
+                            <div class="contianer-writer">
+                                <h1>Diretor</h1>
+                                <p>{{ getDirectorName(tv.credits.crew) }}</p>
+                                <div v-if="tv.credits.crew.length > 0">
+                                    <h1>Escritores</h1>
+                                    <div v-for="(writer, index) in getWriters(tv.credits.crew)" :key="index">
+                                        <p>{{ writer.name }}</p>
+                                    </div>
+                                </div>
+                                <p v-else>Não disponível</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <!-- Aprendendo a usar o bootstrap :) -->
         <div class="container-main d-flex flex-wrap">
             <div class="col-lg-8 col-md-8 col-12">
                 <div class="panel">
@@ -116,7 +116,9 @@ const getNameOringinal = () => {
                         <div v-for="(castMember) in tv.credits.crew.slice(0, showMoreCast ? undefined : maxVisibleCastMembers)"
                             :key="castMember.id" class="mb-3 col-lg-3 col-sm-4 col-6 wid-card">
                             <div class="h-100 card">
-                                <img :src="`https://image.tmdb.org/t/p/w500${castMember.profile_path}`" />
+                                <img v-if="castMember.profile_path"
+                                    :src="`https://image.tmdb.org/t/p/w500${castMember.profile_path}`" />
+                                <img v-else src="@/assets/img/fallback.jpg" alt="Fallback Image" />
                                 <div class="card-body">
                                     <h2 class="name-member">{{ castMember.name }}</h2>
                                     <p class="mt-1 char">{{ castMember.character }}</p>
@@ -153,7 +155,9 @@ const getNameOringinal = () => {
                     <div class="info-item ">
                         <h1>Emissora</h1>
                         <p class="mt-4">
-                            ${{  }}
+                            <img v-if="tv.networks && tv.networks.length > 0"
+                                :src="`https://image.tmdb.org/t/p/w200${tv.networks[0].logo_path}`"
+                                :alt="tv.networks[0].name" />
                         </p>
                     </div>
                     <div class="info-item ">
@@ -180,20 +184,22 @@ const getNameOringinal = () => {
 </template>
 
 <style scoped>
-
-.button-alg{
+.button-alg {
     background-color: #666;
     padding: 10px;
     border-radius: 10px;
 }
+
 .container-main {
     max-width: 1400px;
     clear: both;
     margin: 0 auto;
 }
-.info-opc{
-font-size: 20px;
+
+.info-opc {
+    font-size: 20px;
 }
+
 .keyword-tag {
     background-color: #ddd;
     color: #666;
@@ -213,56 +219,59 @@ font-size: 20px;
     font-size: 21px;
     margin-left: 5px;
 }
-.btn-1{
-        background-color: #fff;
-        padding: 0.4em 1em;
-        border-radius: 20em;
-        border: none;
-        user-select: none;
-        cursor: pointer;
-        font-size: 20px;
-        transition: all 100ms linear;
-        color: #000000;
-        position: relative;
-        box-shadow: inset 0 4px 6px -1px rgb(0 0 0 / 0.1), 
+
+.btn-1 {
+    background-color: #fff;
+    padding: 0.4em 1em;
+    border-radius: 20em;
+    border: none;
+    user-select: none;
+    cursor: pointer;
+    font-size: 20px;
+    transition: all 100ms linear;
+    color: #000000;
+    position: relative;
+    box-shadow: inset 0 4px 6px -1px rgb(0 0 0 / 0.1),
         0 2px 4px -2px rgb(0 0 0 / 0.1);
-    }
+}
 
-    .btn-1:active {
-        box-shadow: inset 0 0 0 4px #043b3f6b;
-    }
+.btn-1:active {
+    box-shadow: inset 0 0 0 4px #043b3f6b;
+}
 
-    .btn-1::before {
-        content: "";
-        width: 100%;
-        height: 100%;
-        background-color: #fff;
-        position: absolute;
-        border-radius: inherit;
-        z-index: -1;
-        top: 0;
-        left: 0;
-        transition: all 500ms ease-in-out;
-        opacity: 0;
-        animation: flash 100ms ease-in-out;
-        transform: scaleX(2) scaleY(2);
-    }
+.btn-1::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    position: absolute;
+    border-radius: inherit;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    transition: all 500ms ease-in-out;
+    opacity: 0;
+    animation: flash 100ms ease-in-out;
+    transform: scaleX(2) scaleY(2);
+}
 
-    .btn-1:hover::before {
+.btn-1:hover::before {
+    opacity: 1;
+    transform: scaleX(1) scaleY(1);
+}
+
+@keyframes flash {
+    0% {
         opacity: 1;
-        transform: scaleX(1) scaleY(1);
+        transform: translate(45px);
     }
 
-    @keyframes flash {
-        0%{
-            opacity: 1;
-            transform: translate(45px);
-        }
-        100%{
-            opacity: 0;
-            transform: translate(0);
-        }
+    100% {
+        opacity: 0;
+        transform: translate(0);
     }
+}
+
 .card {
     position: relative;
     display: flex;
@@ -295,6 +304,12 @@ font-size: 20px;
 
 .name-member {
     font-size: 15px;
+}
+
+.contianer-writer {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 50px;
 }
 
 .container-so {
@@ -330,9 +345,11 @@ font-size: 20px;
     margin-top: 4.5em;
 
 }
-.avi{
+
+.avi {
     font-size: 20px;
 }
+
 .sinopse {
     font-size: 25px;
     font-weight: bold;
@@ -379,5 +396,4 @@ font-size: 20px;
     font-size: 18px;
     font-weight: bold;
     color: #fff;
-}
-</style>
+}</style>
