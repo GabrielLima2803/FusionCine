05b5181dd4e735adf91291178693e1ea0e514230
GabrielLima2803 <gabriellima2803@gmail.com>
2023-11-27 11:29:44 -0300

    Fc - Add Component BoxSearch

---
+++ src/components/card/BoxSearch.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { useSearchStore } from '@/stores/search'
+import { onMounted } from 'vue';
+const searchStore = useSearchStore()
+// 
+
+</script>
+
+<template>
+  <div class="box-search">
+    <table>
+      <tr>
+        <th class="result">Resultado da busca</th>
+      </tr>
+      <tr>
+        <router-link to="/search" class="clear">
+          <td>Filmes</td>
+        </router-link>
+        <router-link to="/searchTv" class="clear">
+          <td>Séries</td>
+        </router-link>
+        <router-link to="/searchPerson" class="clear">
+          <td>Pessoa</td>
+        </router-link>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+.clear{
+  text-decoration: none;
+  color: black;
+}
+.result {
+  background-color: gray;
+  color: white;
+  padding: 1rem;
+  font-size: 20px;
+}
+
+th,
+td {
+  padding: 10px;
+  text-align: left;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  cursor: pointer;
+}
+td:hover {
+  background-color: rgb(219, 217, 217);
+  transition: 0.2s;
+}
+
+table {
+  width: 18%;
+  border-collapse: collapse;
+  margin-top: 110px;
+  border-radius: 10px 10px;
+  border: solid 1px black;
+}
+</style>
