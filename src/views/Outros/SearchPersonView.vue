e4b6d9f69148b5123bfbffb9cd000096b0b16839
GabrielLima2803 <gabriellima2803@gmail.com>
2023-11-27 11:31:55 -0300

    Fc - Add New View SearchPerson

---
+++ src/views/Outros/SearchPersonView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderPrincipal from '@/components/header/HeaderPrincipal.vue';
+import FullFooter from '@/components/footer/FullFooter.vue';
+import CardSearchPerson from '@/components/card/CardSearchPerson.vue';
+
+</script>
+
+<template>
+    <header-principal/>
+    <card-search-person/>
+    <full-footer/>
+</template>
+
+<style scoped>
+
+</style>
