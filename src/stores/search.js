abbe073cc8c544526592dcf70f1da1be4ef7b277
GabrielLima2803 <gabriellima2803@gmail.com>
2023-11-29 16:14:19 -0300

    Fc - Add currentPages

--- src/stores/search.js
+++ src/stores/search.js
@@ -10,6 +10,8 @@ export const useSearchStore = defineStore('search',
         searchPerson: [],
         query: '',
         currentPage: 0,
+        currentPageTv: 0,
+        currentPagePerson: 0,
     })
     const search = computed(() => state.search)
     const searchTv = computed(() => state.searchTv)
@@ -18,30 +20,45 @@ export const useSearchStore = defineStore('search',
     const SearchMulti = async (param, page = 1) => {
         const response = await api.get(`/search/movie?query=${param}&include_adult=false&language=pt-BR&page=${page}`);
         console.log(response.data)
+        console.log(state.currentPage)
         state.currentPage = page;
         state.search = response.data.results;
     }
     const SearchTv = async (param, page = 1) => {
         const response = await api.get(`/search/tv?query=${param}&include_adult=false&language=pt-BR&page=${page}`);
         console.log(response.data)
-        state.currentPage = page;
+        state.currentPageTv = page;
         state.searchTv = response.data.results;
     }
     const SearchPerson = async (param, page = 1) => {
         const response = await api.get(`/search/person?query=${param}&include_adult=false&language=pt-BR&page=${page}`);
         console.log(response.data)
-        state.currentPage = page;
+        state.currentPagePerson = page;
         state.searchPerson = response.data.results;
     }
     const nextPage = () => {
         const nextPage = state.currentPage + 1;
         SearchMulti(nextPage); 
+        SearchTv(nextPage);
+        SearchPerson(nextPage);
     }
 
     const backPage = () => {
         const backPage = state.currentPage - 1;
         SearchMulti(backPage); 
+        SearchTv(backPage); 
+        SearchPerson(backPage); 
 
     }
-    return {search, SearchMulti, nextPage, backPage, SearchTv, searchTv, searchPerson, SearchPerson}
-})
+
+    return {
+        search,
+        SearchMulti,
+        nextPage,
+        backPage,
+        SearchTv,
+        searchTv,
+        currentPage: state.currentPage, 
+        searchPerson,
+        SearchPerson,
+    }})
