2d078914d9a61623e290627cc1a546ac59ec088f
anaclarag <gomes06anaclara@gmail.com>
2023-12-01 15:12:52 -0300

    Página Séries solo

--- src/views/Filmes&Series/SoloSerieView.vue
+++ src/views/Filmes&Series/SoloSerieView.vue
@@ -61,339 +61,388 @@ const getWriters = (crew) => {
 </script>
 
 <template>
-    <header-principal />
-    <div v-for="tv in tvs" :key="tv.id">
-        <div class="backdrop">
-            <div class="backdrop"
-                :style="{ 'background-image': `linear-gradient(to right, rgb(16, 14, 14) 150px, rgba(16, 14, 14, 0.84) 100%),url(https://image.tmdb.org/t/p/original${tv.backdrop_path})` }">
-                <div class="container-main">
-                    <div class="container-tv z-3">
-                        <img :src="`https://image.tmdb.org/t/p/w500${tv.poster_path}`" :alt="tv.title" width="350"
-                            class="img-tv" />
-                        <div class="info-tv">
-                            <h1 class="title">{{ tv.name }}</h1>
-                            <div class="container-so">
-                                <p class="tv-date mt-3 mb-3">{{ }} </p>
-                                <p class="mt-3 ml-3"> • {{ getGenreNames(tv.genres) }} </p>
-                            </div>
-                            <div class="trailer mt-5">
-                                <button @click="openTrailer(tv.videos && tv.videos.results)">
-                                    <i class="bi bi-play-fill text-aling"></i> Reproduzir trailer
-                                </button>
-                            </div>
-                            <div class="avi mt-6 d-flex">
-                                <i class="bi bi-star-fill"></i>
-                                <p class="ml-4"> {{ tv.vote_average }}
-                                </p>
-                            </div>
-                            <h2 class="sinopse mt-7 ">Sinopse</h2>
-                            <p class="mt-3">{{ tv.overview }}</p>
-                            <div class="btn-alugar mt-6 info-opc">
-                                <i class="bi bi-heart-fill"></i>
-                                <button class="ml-3 btn-1">Alugar</button>
-                            </div>
-                            <div class="contianer-writer">
-                                <h1>Diretor</h1>
-                                <p>{{ getDirectorName(tv.credits.crew) }}</p>
-                                <div v-if="tv.credits.crew.length > 0">
-                                    <h1>Escritores</h1>
-                                    <div v-for="(writer, index) in getWriters(tv.credits.crew)" :key="index">
-                                        <p>{{ writer.name }}</p>
-                                    </div>
-                                </div>
-                                <p v-else>Não disponível</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<header-principal />
+  <div v-for="serie in tvs" :key="serie.id">
+    <div class="backdrop">
+      <div class="backdrop" :style="{
+        'background-image': `linear-gradient(to right, rgb(16, 14, 14) 150px, rgba(16, 14, 14, 0.84) 100%),url(https://image.tmdb.org/t/p/original${serie.backdrop_path})`
+      }">
+        <div class="container-main">
+          <div class="container-serie z-3">
+            <img :src="`https://image.tmdb.org/t/p/w500${serie.poster_path}`" :alt="serie.title" width="350"
+              class="img-serie" />
+            <div class="info-serie">
+              <h1 class="title">{{ serie.title }}</h1>
             </div>
+          </div>
+          <div class="trailer mt-5">
+            <button @click="openTrailer(serie.videos && serie.videos.results)">
+              <i class="bi bi-play"></i> Reproduzir trailer
+            </button>
+          </div>
+          <div class="btn-alugar mt-6 info-opc">
+            <i class="bi bi-heart"></i>
+            <button class="ml-3"> Alugar </button>
+          </div>
+          <div class="avi mt-6 d-flex">
+            <i class="bi bi-star-fill"></i>
+            <p class="ml-4"> {{ serie.vote_average }}</p>
+          </div>
         </div>
-        <div class="container-main d-flex flex-wrap">
-            <div class="col-lg-8 col-md-8 col-12">
-                <div class="panel">
-                    <h1 class="mb-4 font-weight-bold text-center text-md-left mt-4 title-member">Elenco Principal</h1>
-                    <div class="d-flex flex-wrap">
-                        <div v-for="(castMember) in tv.credits.crew.slice(0, showMoreCast ? undefined : maxVisibleCastMembers)"
-                            :key="castMember.id" class="mb-3 col-lg-3 col-sm-4 col-6 wid-card">
-                            <div class="h-100 card">
-                                <img v-if="castMember.profile_path"
-                                    :src="`https://image.tmdb.org/t/p/w500${castMember.profile_path}`" />
-                                <img v-else src="@/assets/img/fallback.jpg" alt="Fallback Image" />
-                                <div class="card-body">
-                                    <h2 class="name-member">{{ castMember.name }}</h2>
-                                    <p class="mt-1 char">{{ castMember.character }}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="text-center">
-                        <button class="btn-show-cast btn-lg" @click="toggleShowMore">
-                            {{ showMoreCast ? 'ver menos' : 'ver mais' }}
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-12">
-                <div class="info text-center  text-md-left">
-                    <div class="social d-flex mt-4 title-member">
-                        <i class="bi bi-instagram"></i>
-                        <i class="bi bi-twitter ml-5"></i>
-                        <i class="bi bi-facebook ml-5"></i>
-                    </div>
-                    <div class="info-item ">
-                        <h1>Título original</h1>
-                        <p class="mt-4">
-                            {{ tv.original_name }}
-                        </p>
-                    </div>
-                    <div class="info-item ">
-                        <h1>Situação</h1>
-                        <p class="mt-4">
-                            {{ tv.status }}
-                        </p>
-                    </div>
-                    <div class="info-item ">
-                        <h1>Emissora</h1>
-                        <p class="mt-4">
-                            <img v-if="tv.networks && tv.networks.length > 0"
-                                :src="`https://image.tmdb.org/t/p/w200${tv.networks[0].logo_path}`"
-                                :alt="tv.networks[0].name" />
-                        </p>
-                    </div>
-                    <div class="info-item ">
-                        <h1>Idiama Original</h1>
-                        <p class="mt-4">
-                            {{ tv.languages }}
-                        </p>
-                    </div>
-                    <div class="info-item">
-                        <h1>Palavras-chave</h1>
-                        <div v-for="(keyword, index) in getKeywordsNames(tv.keywords)" :key="index"
-                            class="d-flex mt-2 flex-wrap justify-content-center justify-content-md-start">
-                            <p class="keyword-tag">
-                                {{ keyword }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <card-pop />
-        </div>
+      </div>
     </div>
-    <full-footer />
+    <hr />
+    <div class="container-main d-flex flex-wrap">
+      <!-- Elenco -->
+      <div>
+        <div class="panel">
+          <h2 class="mb-4 font-weight-bold text-center text-md-left mt-4 title-member">Elenco Principal</h2>
+          <div class="d-flex flex-wrap">
+  <div v-for="(castMember) in serie.credits.cast.slice(0, showMoreCast ? undefined : maxVisibleCastMembers)"
+    :key="castMember.id" class="m-3 wid-card">
+    <div class="card-container">
+      <div class="circle">
+        <img v-if="castMember.profile_path" :src="`https://image.tmdb.org/t/p/w500${castMember.profile_path}`"
+          class="cast-member-image mask full mx-auto" />
+        <img v-else src="@/assets/img/fallback.jpg" alt="Fallback Image" class="cast-member-image mask full" />
+      </div>
+      <div class="card-details">
+        <p class="name-member">{{ castMember.name }}</p>
+        <p class="char">{{ castMember.character }}</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="text-center">
+  <button class="btn-show-cast btn-lg d-flex justify-content-center align-items-center" @click="toggleShowMore">
+    <i v-if="showMoreCast" class="bi bi-arrow-bar-up"></i>
+    <i v-else class="bi bi-arrow-bar-down"></i>
+  </button>
+</div>
+        </div>
+      </div>
+    </div>
+    <!-- Informações -->
+    <div class="info-gener">
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="info text-center text-md-left m-20">
+          <h2 class="sinopse mt-4">Sinopse</h2>
+          <div class="info-sobre">
+            <h5 class="title">{{ serie.title }}</h5>
+            <h6 class="mt-3"> {{ getGenreNames(serie.genres) }}</h6>
+            <div class="sinop">
+              <p class="mt-3">{{ serie.overview }}</p>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <div class="info-item p-20">
+          <h1 class="m-20">Título original</h1>
+          <h6 class="mt-4 ml-5">{{ serie.original_title }}</h6>
+        </div>
+      </div>
+      <!-- Adicione estas classes à div .informacoes -->
+      <div class="informacoes">
+        <div class="info-col">
+          <div class="info-item">
+            <h2>Sobre o filme</h2>
+          </div>
+          <div class="info-item">
+            <h6>Lançamento</h6>
+            <!-- <p class="serie-date mt-1">{{ formatDate(serie.release_date) }}</p> -->
+          </div>
+          <div class="info-item">
+            <h6>Duração</h6>
+            <!-- <p class="mt-1">{{ getFormattedRuntime(serie.runtime) }}</p> -->
+          </div>
+          <div class="info-item">
+            <h6>Genêro</h6>
+            <p class="mt-1">{{ getGenreNames(serie.genres) }}</p>
+          </div>
+        </div>
+
+        <div class="info-col">
+          <div class="info-item">
+            <h2>Informações</h2>
+          </div>
+          <div class="info-item">
+            <h6>Situação</h6>
+            <p class="mt-1">{{ serie.status }}</p>
+          </div>
+          <div class="info-item">
+            <h6>Orçamento</h6>
+            <p class="mt-1">${{ serie.budget }}</p>
+          </div>
+          <div class="info-item">
+            <h6>Receita</h6>
+            <p class="mt-1">${{ serie.revenue }}</p>
+          </div>
+        </div>
+
+        <div class="info-col">
+          <div class="info-item">
+            <h2>Palavras-chave</h2>
+          </div>
+          <div class="keyword-grid">
+            <div v-for="(keyword, index) in getKeywordsNames(serie.keywords)" :key="index" class="keyword-item">
+              <p class="keyword-tag">{{ keyword }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <full-footer />
+    </div>
+  </div>
+         
 </template>
 
 <style scoped>
-.button-alg {
-    background-color: #666;
-    padding: 10px;
-    border-radius: 10px;
-}
-
 .container-main {
-    max-width: 1400px;
-    clear: both;
-    margin: 0 auto;
+ max-width: 1400px;
+ clear: both;
+ margin: 0 auto;
 }
 
 .info-opc {
-    font-size: 20px;
+ font-size: 18px;
+ color: black;
+ width: 200px;
+ height: 40px;
+ background-color: #fff;
+ border-radius: 5px;
+ padding: 10px;
 }
-
-.keyword-tag {
-    background-color: #ddd;
-    color: #666;
-    padding: 4px 8px;
-    border-radius: 6px;
-    margin-right: 10px;
-    font-size: 18px;
-}
-
-.info-item>h1 {
-    margin-top: 20px;
-    font-size: 30px;
-    font-weight: bold;
-}
-
-.info-item>p {
-    font-size: 21px;
-    margin-left: 5px;
-}
-
-.btn-1 {
-    background-color: #fff;
-    padding: 0.4em 1em;
-    border-radius: 20em;
-    border: none;
-    user-select: none;
-    cursor: pointer;
-    font-size: 20px;
-    transition: all 100ms linear;
-    color: #000000;
-    position: relative;
-    box-shadow: inset 0 4px 6px -1px rgb(0 0 0 / 0.1),
-        0 2px 4px -2px rgb(0 0 0 / 0.1);
-}
-
-.btn-1:active {
-    box-shadow: inset 0 0 0 4px #043b3f6b;
-}
-
-.btn-1::before {
-    content: "";
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-    position: absolute;
-    border-radius: inherit;
-    z-index: -1;
-    top: 0;
-    left: 0;
-    transition: all 500ms ease-in-out;
-    opacity: 0;
-    animation: flash 100ms ease-in-out;
-    transform: scaleX(2) scaleY(2);
-}
-
-.btn-1:hover::before {
-    opacity: 1;
-    transform: scaleX(1) scaleY(1);
-}
-
-@keyframes flash {
-    0% {
-        opacity: 1;
-        transform: translate(45px);
-    }
-
-    100% {
-        opacity: 0;
-        transform: translate(0);
-    }
-}
-
-.card {
-    position: relative;
-    display: flex;
-    width: 160px;
-    flex-direction: column;
-    cursor: pointer;
-    min-width: 0;
-    word-wrap: break-word;
-    background-color: #fff;
-    background-clip: border-box;
-    border: 1px solid rgba(0, 0, 0, .125);
-    margin-left: 0.5rem;
-    border-radius: 0.5rem;
-}
-
-.card-body {
-    padding: 10px;
-    flex: 1 1 auto;
-    min-height: 1px;
-}
-
-.char {
-    color: gray;
-    font-size: 12px;
-}
-
-.title-member {
-    font-size: 30px;
-}
-
-.name-member {
-    font-size: 15px;
-}
-
-.contianer-writer {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 50px;
-}
-
 .container-so {
-    display: flex;
-    font-size: 18px;
-    font-weight: bold;
+ display: flex;
+ font-size: 18px;
+ font-weight: bold;
 }
 
-.container-tv {
-    display: flex;
-    color: white;
+.container-movie {
+ display: flex;
+ color: white;
+
 
 }
+
 
 .title {
-    font-weight: bold;
-    font-size: 42px;
+ font-weight: bold;
+ font-size: 42px;
 }
+
 
 .trailer {
-    font-size: 22px;
-    font-weight: bold;
+ font-size: 18px;
+ color: black;
+ width: 200px;
+ height: 40px;
+ background-color: #fff;
+ border-radius: 5px;
+ padding: 10px;
 }
 
-.img-tv {
-    border-radius: 10px;
-    margin-top: 3.5em;
-    margin-bottom: 3.5em;
+hr {
+ width: 95%;
+ margin: 20px;
 }
 
-.info-tv {
-    padding: 15px;
-    margin-top: 4.5em;
+.img-movie {
+ border-radius: 10px;
+ margin-top: 3.5em;
+ margin-bottom: 3.5em;
+}
+
+.info-movie {
+ padding: 15px;
+ margin-top: 4.5em;
+
 
 }
 
 .avi {
-    font-size: 20px;
+ font-size: 20px;
 }
-
-.sinopse {
-    font-size: 25px;
-    font-weight: bold;
-}
-
 .backdrop {
-    background-position: center top;
-    background-repeat: no-repeat;
-    background-size: cover;
-    min-height: 440px;
-}
-
-.circle {
-    width: 50px;
-    height: 50px;
-    background-color: #ccc;
-    border-radius: 50%;
-    position: relative;
-}
-
-.mask {
-    clip: rect(0, 50px, 100px, 0);
-    position: absolute;
-}
-
-.full {
-    transform: rotate(0deg);
+ background-position: center top;
+ background-repeat: no-repeat;
+ background-size: cover;
+ min-height: 440px;
 }
 
 .half {
-    transform: rotate(180deg);
+ transform: rotate(180deg);
 }
+
 
 .fill {
-    background-color: #f8cc1d;
-    clip: rect(0, 100px, 100px, 50px);
+ background-color: #f8cc1d;
+ clip: rect(0, 100px, 100px, 50px);
 }
 
-.inside-circle {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 18px;
-    font-weight: bold;
-    color: #fff;
-}</style>
+
+.cast-member-image {
+ max-width: 100%;
+}
+
+
+.mask {
+ clip: rect(0, 100px, 100px, 0);
+ position: absolute;
+}
+
+.full {
+ transform: rotate(0deg);
+}
+
+
+.card-container {
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+}
+
+
+.card-details {
+ text-align: center;
+ margin-top: 10px;
+}
+
+
+.name-member {
+ font-size: 15px;
+ margin-bottom: 5px;
+}
+
+.char {
+ color: gray;
+ font-size: 12px;
+}
+
+
+.circle {
+ position: relative;
+ width: 100px;
+ height: 100px;
+ background-color: #ccc;
+ border-radius: 50%;
+ overflow: hidden;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ margin-bottom: 10px;
+ cursor: pointer;
+ box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+}
+
+.circle:hover {
+ border: 2px solid rgba(0, 0, 0, 0.797);
+ transform: scale(1.05);
+ transition: transform 0.3s ease;
+}
+
+.sinopse {
+ font-size: 25px;
+ font-weight: bold;
+ margin-top: 40px;
+ margin-bottom: 10px;
+ padding-top: 30px;
+ padding-bottom: 5px;
+ padding-left: 20px;
+}
+
+
+.informacoes {
+ display: grid;
+ grid-template-columns: repeat(3, 1fr);
+ justify-content: space-between;
+ margin: 20px;
+}
+
+
+.info-col {
+ width: 50%;
+ align-items: center;
+}
+
+
+
+
+.info-gener {
+ background-color: #bfbfbf3c;
+}
+
+.info-sobre {
+ border: 1px solid rgba(0, 0, 0, .125);
+ padding: 14px 16px;
+ border-radius: 12px;
+ background-color: #fff;
+ color: #9b9a9a;
+ margin: 20px;
+}
+
+
+.info-sobre>h5 {
+ color: black;
+ font-size: 20px;
+}
+
+.info-sobre>h6 {
+ color: rgba(160, 160, 160, 0.879);
+ font-size: 10px;
+}
+
+
+.info-item>h1 {
+ margin: 20px;
+ font-size: 20px;
+ font-weight: bold;
+}
+
+
+.info-item {
+ margin: 10px;
+}
+
+.info-item>p {
+ font-size: 11px;
+ margin: 20px;
+ color: #666;
+}
+
+
+.info-item>h6 {
+ font-size: 13px;
+ margin-left: 5px;
+ color: black;
+}
+
+
+.card-body {
+ padding: 10px;
+ flex: 1 1 auto;
+ min-height: 1px;
+}
+
+
+.keyword-grid {
+ display: grid;
+ grid-template-columns: repeat(2, 1fr);
+ grid-gap: 10px;
+}
+
+.keyword-item {
+ background-color: #f5f5f5;
+ padding: 5px;
+ border-radius: 5px;
+ text-align: center;
+}
+
+
+.keyword-tag {
+ font-size: 12px;
+ margin: 0;
+ color: #333;
+}
+
+</style>
